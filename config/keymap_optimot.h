4ae694657afbe1e0818198f54950da593f42784c
Jérémy Chatard <jchatard@breek.fr>
2023-03-14 14:14:43 +0100

    Fix Optimot file.
    It was based on a really old version.

--- config/keymap_optimot.h
+++ config/keymap_optimot.h
@@ -30,57 +30,56 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define OP_DLR  GRAVE  // $
+#define OP_DLR  GRAVE // $
 #define OP_LDAQ N1    // «
 #define OP_RDAQ N2    // »
 #define OP_DQUO N3    // "
-#define OP_LPRN N4    // (
-#define OP_RPRN N5    // )
-#define OP_PERC N6    // %
-#define OP_PLUS N7    // +
-#define OP_MINS N8    // -
-#define OP_EQL  N9    // =
-#define OP_SLSH N0    // /
-#define OP_ASTR KP_MINUS // *
-#define OP_HASH KP_EQUAL  // #
+#define OP_MIN  N4    // -
+#define OP_PLUS N5    // +
+#define OP_ASTR N6    // *
+#define OP_SLSH N7    // /
+#define OP_EQL  N8    // =
+#define OP_LPRN N9    // (
+#define OP_RPRN N0    // )
+#define OP_AT   MINUS // @
+#define OP_HASH EQUAL // #
 // Row 2
-#define OP_AGRV Q    // À
-#define OP_J    W    // J
-#define OP_O    E    // O
-#define OP_EACU R    // É
-#define OP_B    T    // B
-#define OP_F    Y    // F
-#define OP_D    U    // D
-#define OP_L    I    // L
-#define OP_QUOT O    // '
-#define OP_Q    P    // Q
-#define OP_X    LEFT_BRACKET // X
-#define OP_CCED LEFT_BRACKET // Ç
+#define OP_AGRV Q     // À
+#define OP_J    W     // J
+#define OP_O    E     // O
+#define OP_EACU R     // É
+#define OP_B    T     // B
+#define OP_F    Y     // F
+#define OP_D    U     // D
+#define OP_L    I     // L
+#define OP_QUOT O     // '
+#define OP_Q    P     // Q
+#define OP_X    LBKT  // X
+#define OP_CCED RBKT  // Ç
 // Row 3
-#define OP_A    A    // A
-#define OP_I    S    // I
-#define OP_E    D    // E
-#define OP_U    F    // U
-#define OP_COMM G    // ,
-#define OP_P    H    // P
-#define OP_T    J    // T
-#define OP_S    K    // S
-#define OP_R    L    // R
-#define OP_N    SEMICOLON // N
-#define OP_DCIR SINGLE_QUOTE // ^
-#define OP_Z    BACKSLASH // Z
+#define OP_A    A     // A
+#define OP_I    S     // I
+#define OP_E    D     // E
+#define OP_U    F     // U
+#define OP_COMM G     // ,
+#define OP_P    H     // P
+#define OP_T    J     // T
+#define OP_S    K     // S
+#define OP_R    L     // R
+#define OP_N    SEMI  // N
+#define OP_CIR  SQT   // ^
+#define OP_Z    BSLH  // Z
 // Row 4
-#define OP_K    NON_US_BACKSLASH // K
-#define OP_Y    Z    // Y
-#define OP_EGRV X    // È
-#define OP_DOT  C    // .
-#define OP_W    V    // W
-#define OP_DEL  BACKSPACE // Backspace
-#define OP_G    N    // G
-#define OP_C    M    // C
-#define OP_M    KP_COMMA // M
-#define OP_H    DOT  // H
-#define OP_V    KP_SLASH // V
+#define OP_K    Z     // K
+#define OP_Y    X     // Y
+#define OP_EGRV C     // È
+#define OP_DOT  V     // .
+#define OP_W    B     // W
+#define OP_G    N     // G
+#define OP_C    M     // C
+#define OP_M    COMMA // M
+#define OP_H    DOT   // H
+#define OP_V    FSLH  // V
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -96,19 +95,19 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define OP_EURO LS(OP_DLR)  // €
-#define OP_1    LS(OP_LDAQ) // 1
-#define OP_2    LS(OP_RDAQ) // 2
-#define OP_3    LS(OP_DQUO) // 3
-#define OP_4    LS(OP_LPRN) // 4
-#define OP_5    LS(OP_RPRN) // 5
-#define OP_6    LS(OP_PERC) // 6
-#define OP_7    LS(OP_PLUS) // 7
-#define OP_8    LS(OP_MINS) // 8
-#define OP_9    LS(OP_EQL)  // 9
-#define OP_0    LS(OP_SLSH) // 0
-#define OP_UNDS LS(OP_ASTR) // _
-#define OP_MORD LS(OP_HASH) // º
+#define OP_EURO  LS(OP_DLR)  // €
+#define OP_1     LS(OP_LDAQ) // 1
+#define OP_2     LS(OP_RDAQ) // 2
+#define OP_3     LS(OP_DQUO) // 3
+#define OP_4     LS(OP_MIN)  // 4
+#define OP_5     LS(OP_PLUS) // 5
+#define OP_6     LS(OP_ASTR) // 6
+#define OP_7     LS(OP_SLSH) // 7
+#define OP_8     LS(OP_EQL)  // 8
+#define OP_9     LS(OP_LPRN) // 9
+#define OP_0     LS(OP_RPRN) // 0
+#define OP_UNDS  LS(OP_AT)   // _
+#define OP_DEGRE LA(OP_HASH) // º
 // Row 2
 #define OP_QUES LS(OP_QUOT) // ?
 // Row 3
@@ -140,7 +139,7 @@
 #define OP_RBRC LA(OP_RPRN) // ]
 #define OP_AT   LA(OP_PERC) // @
 #define OP_PLMN LA(OP_PLUS) // ±
-#define OP_MMNS LA(OP_MINS) // −
+#define OP_MMNS LA(OP_MIN) // −
 #define OP_NEQL LA(OP_EQL)  // ≠
 #define OP_BSLS LA(OP_SLSH) // \ (backslash)
 #define OP_MUL  LA(OP_ASTR) // ×
@@ -206,7 +205,7 @@
 #define OP_TTRD LS(LA(OP_RPRN)) // ⅔
 #define OP_PRIM LS(LA(OP_PERC)) // ′
 #define OP_DPRM LS(LA(OP_PLUS)) // ″
-#define OP_NBHY LS(LA(OP_MINS)) // ‑
+#define OP_NBHY LS(LA(OP_MIN)) // ‑
 #define OP_AEQL LS(LA(OP_EQL))  // ≈
 #define OP_DIV  LS(LA(OP_SLSH)) // ÷
 #define OP_OVER LS(LA(OP_ASTR)) // ‾
